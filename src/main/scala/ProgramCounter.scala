3805b82fc7a34fded7ecdccde17d9d6611b03d6d
Carlos de Paula <me@carlosedp.com>
2021-09-29 18:37:02 -0300

    Add param for PC start address

--- src/main/scala/ProgramCounter.scala
+++ src/main/scala/ProgramCounter.scala
@@ -8,11 +8,11 @@ class PCPort(bitWidth: Int = 32) extends Bundle {
   val countEnable = Input(Bool()) // 1 => COUNT UP, 0 => STOPPED
 }
 
-class ProgramCounter(regWidth: Int = 32) extends Module {
+class ProgramCounter(regWidth: Int = 32, entryPoint: Long = 0) extends Module {
   val io = IO(new Bundle {
     val pcPort = new PCPort(regWidth)
   })
-  val pc = RegInit(0.U(regWidth.W))
+  val pc = RegInit(entryPoint.U(regWidth.W))
 
   when(io.pcPort.writeEnable) {
     pc := Mux(io.pcPort.writeAdd, pc + io.pcPort.dataIn - 4.U, io.pcPort.dataIn)
