6d740821e7f0c14e4c89c43267e93f7daca5de15
Carlos de Paula <me@carlosedp.com>
2021-09-29 15:16:47 -0300

    Improve decode tests

--- src/test/scala/DecoderSpec.scala
+++ src/test/scala/DecoderSpec.scala
@@ -11,29 +11,18 @@ class DecoderSpec extends AnyFlatSpec with ChiselScalatestTester with should.Mat
   "Decoder" should "Decode an ADD instruction (type R)" in {
     test(new Decoder()) { c =>
       //  Template: "b0000000??????????000?????0110011"
-      c.io.DecoderPort.op.poke("b00000000000100001000000010110011".U)
+      val genInst = InstructionHelper(ADD, 1, 2, 3, 0)
+      c.io.DecoderPort.op.poke(genInst.U)
       c.clock.step()
-      c.io.DecoderPort.inst.expect(ADD)
-      c.io.DecoderPort.rd.peek().litValue() should be(1)
-      c.io.DecoderPort.rs1.peek().litValue() should be(1)
-      c.io.DecoderPort.rs2.peek().litValue() should be(1)
-      c.io.DecoderPort.imm.peek().litValue() should be(0)
-      c.io.DecoderPort.toALU.expect(true.B)
-      c.io.DecoderPort.branch.expect(false.B)
+      validateResult(c, ADD, 1, 2, 3, 0, true.B, false.B)
     }
   }
   it should "Decode an ANDI instruction (type I)" in {
     test(new Decoder()) { c =>
-      //  Template: b?????????????????111?????0010011
-      c.io.DecoderPort.op.poke("b11111111111111000111001110010011".U)
+      val genInst = InstructionHelper(ANDI, 7, 24, 0, -1)
+      c.io.DecoderPort.op.poke(genInst.U)
       c.clock.step()
-      c.io.DecoderPort.inst.expect(ANDI)
-      c.io.DecoderPort.rd.peek().litValue() should be(7)
-      c.io.DecoderPort.rs1.peek().litValue() should be(24)
-      c.io.DecoderPort.rs2.peek().litValue() should be(0)
-      c.io.DecoderPort.imm.peek().litValue().toLong.toBinaryString should be("11111111111111111111111111111111")
-      c.io.DecoderPort.toALU.expect(true.B)
-      c.io.DecoderPort.branch.expect(false.B)
+      validateResult(c, ANDI, 7, 24, 0, -1, true.B, false.B)
     }
   }
   it should "Decode an SB instruction (type S)" in {
@@ -41,15 +30,7 @@ class DecoderSpec extends AnyFlatSpec with ChiselScalatestTester with should.Mat
       //  Template: b?????????????????000?????0100011
       c.io.DecoderPort.op.poke("b11111010101010101000011110100011".U)
       c.clock.step()
-      c.io.DecoderPort.inst.expect(SB)
-      c.io.DecoderPort.rd.peek().litValue() should be(0)
-      c.io.DecoderPort.rs1.peek().litValue() should be(21)
-      c.io.DecoderPort.rs2.peek().litValue() should be(10)
-      c.io.DecoderPort.imm.peek().litValue().toLong.toBinaryString should be(
-        "11111111111111111111111110101111"
-      )
-      c.io.DecoderPort.toALU.expect(false.B)
-      c.io.DecoderPort.branch.expect(false.B)
+      validateResult(c, SB, 0, 21, 10, -81, false.B, false.B)
     }
   }
   it should "Decode an BEQ instruction (type B)" in {
@@ -58,13 +39,7 @@ class DecoderSpec extends AnyFlatSpec with ChiselScalatestTester with should.Mat
       // 11111111111111111111 1 010101 0101 0
       c.io.DecoderPort.op.poke("b10101010101010101000010111100011".U)
       c.clock.step()
-      c.io.DecoderPort.inst.expect(BEQ)
-      c.io.DecoderPort.rd.peek().litValue() should be(0)
-      c.io.DecoderPort.rs1.peek().litValue() should be(21)
-      c.io.DecoderPort.rs2.peek().litValue() should be(10)
-      c.io.DecoderPort.imm.peek().litValue().toLong.toBinaryString should be("11111111111111111111101010101010")
-      c.io.DecoderPort.toALU.expect(false.B)
-      c.io.DecoderPort.branch.expect(true.B)
+      validateResult(c, BEQ, 0, 21, 10, -1366, false.B, true.B)
     }
   }
   it should "Decode an LUI instruction (type U)" in {
@@ -87,13 +62,44 @@ class DecoderSpec extends AnyFlatSpec with ChiselScalatestTester with should.Mat
       // 11111111111 1 01010101 0 1010101010 0
       c.io.DecoderPort.op.poke("b11010101010001010101101011101111".U)
       c.clock.step()
-      c.io.DecoderPort.inst.expect(JAL)
-      c.io.DecoderPort.rd.peek().litValue() should be(21)
-      c.io.DecoderPort.rs1.peek().litValue() should be(0)
-      c.io.DecoderPort.rs2.peek().litValue() should be(0)
-      c.io.DecoderPort.imm.peek().litValue().toLong.toBinaryString should be("11111111111101010101010101010100")
-      c.io.DecoderPort.toALU.expect(false.B)
-      c.io.DecoderPort.branch.expect(false.B)
+      validateResult(c, JAL, 21, 0, 0, -699052, false.B, false.B)
     }
   }
+
+  def validateResult(
+    c: Decoder,
+    inst: Instruction.Type,
+    rd: Int,
+    rs1: Int,
+    rs2: Int,
+    imm: Int,
+    toALU: Bool,
+    branch: Bool
+  ) = {
+    c.io.DecoderPort.inst.expect(inst)
+    c.io.DecoderPort.rd.peek().litValue() should be(rd)
+    c.io.DecoderPort.rs1.peek().litValue() should be(rs1)
+    c.io.DecoderPort.rs2.peek().litValue() should be(rs2)
+    c.io.DecoderPort.imm.peek().litValue().toLong.toBinaryString should be(imm.toBinaryString)
+    c.io.DecoderPort.toALU.expect(toALU)
+    c.io.DecoderPort.branch.expect(branch)
+  }
+
+  def InstructionHelper(inst: Instruction.Type, rd: Int, rs1: Int, rs2: Int, imm: Long): String =
+    inst match {
+      case ADD =>
+        String.format(
+          "b0000000%05d%05d000%05d0110011",
+          rs2.toBinaryString.toInt,
+          rs1.toBinaryString.toInt,
+          rd.toBinaryString.toInt
+        )
+      case ANDI =>
+        String.format(
+          "b%12s%05d111%05d0010011",
+          imm.toBinaryString.takeRight(12),
+          rs1.toBinaryString.toInt,
+          rd.toBinaryString.toInt
+        )
+    }
 }
