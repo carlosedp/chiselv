862ba30afd64aa806ad055de23541c541e0de082
Carlos de Paula <me@carlosedp.com>
2021-10-29 11:04:21 -0300

    Adjust memory test for both memories

--- src/test/scala/MemorySpec.scala
+++ src/test/scala/MemorySpec.scala
@@ -10,7 +10,7 @@ import flatspec._
 import matchers._
 
 class MemorySpec extends AnyFlatSpec with ChiselScalatestTester with should.Matchers {
-  behavior of "Memory"
+  behavior of "DataMemory"
 
   it should "write and read from address" in {
     test(new DualPortRAM(32, 1 * 1024)) { c =>
@@ -34,35 +34,29 @@ class MemorySpec extends AnyFlatSpec with ChiselScalatestTester with should.Matc
       c.io.dualPort.readData.expect(0.U)
     }
   }
-  it should "load from file and read multiple addresses in instruction memory" in {
+
+  behavior of "InstructionMemory"
+  it should "load from file and read multiple instructions" in {
     val filename = "MemorySpecTestFile.hex"
     // Create memory test file with 32bit address space
     new PrintWriter(new File(filename)) { write("00010203\r\n08090A0B\r\nDEADBEEF\r\n07060504\r\n"); close }
 
-    test(new DualPortRAM(32, 16 * 1024, filename)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-      c.io.dualPort.readAddr.poke(0.U)
-      c.io.dualPort.writeAddr.poke(0.U)
-      c.io.dualPort.writeMask.poke("b1111".U)
+    test(new InstructionMemory(32, 16 * 1024, filename)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+      c.io.memPort.readAddr.poke(0.U)
       c.clock.step()
-      c.io.dualPort.readData.expect("h00010203".U)
-      c.io.dualPort.readAddr.poke(4.U)
+      c.io.memPort.readData.expect("h00010203".U)
+      c.io.memPort.readAddr.poke(4.U)
       c.clock.step()
-      c.io.dualPort.readData.expect("h08090A0B".U)
-      c.io.dualPort.readAddr.poke(8.U)
+      c.io.memPort.readData.expect("h08090A0B".U)
+      c.io.memPort.readAddr.poke(8.U)
       c.clock.step()
-      c.io.dualPort.readData.expect("hDEADBEEF".U)
-      c.io.dualPort.readAddr.poke(12.U)
+      c.io.memPort.readData.expect("hDEADBEEF".U)
+      c.io.memPort.readAddr.poke(12.U)
       c.clock.step()
       c.clock.step()
-      c.io.dualPort.readData.expect("h07060504".U)
-      c.io.dualPort.readAddr.poke(1.U)
+      c.io.memPort.readData.expect("h07060504".U)
+      c.io.memPort.readAddr.poke(1.U)
       c.clock.step()
-      c.io.dualPort.writeAddr.poke(1.U)
-      c.io.dualPort.writeEnable.poke(true.B)
-      c.io.dualPort.writeData.poke(1234.U)
-      c.io.dualPort.readAddr.poke(1.U)
-      c.clock.step()
-      c.io.dualPort.readData.expect(1234.U)
     }
     new File(filename).delete()
   }
